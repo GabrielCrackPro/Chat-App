080df967a8039b418a303636882cb5bb535b3415
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-07-01 19:44:42 +0200

    Reply function added 💻

--- index.js
+++ index.js
@@ -7,29 +7,9 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public" + "/index.html");
 });
 
-const getTime = () => {
-  let hours = new Date().getHours();
-  let minutes = new Date().getMinutes();
-  let seconds = new Date().getSeconds();
-
-  if (hours < 10) hours = "0" + hours;
-  if (minutes < 10) minutes = "0" + minutes;
-  if (seconds < 10) seconds = "0" + seconds;
-
-  return `${hours}:${minutes}:${seconds}`;
-};
-let messageHistory = [];
-const getMessageHistory = () => {
-  for (let i = 0; i <= messageHistory.length; i++) {
-    io.emit(
-      "chat message",
-      `[Chat Bot 🤖] ${messageHistory[i]} at ${getTime()}`
-    );
-  }
-};
-
 //Socket connection managment
 let userTotal = 0;
+let messageCount = 0;
 io.on("connection", (socket) => {
   userTotal++;
   console.log(`[Chat Bot 🤖]: user_${userTotal} connected at ${getTime()}`); //When someone loads the app
@@ -48,7 +28,8 @@ io.on("connection", (socket) => {
   });
 
   socket.on("chat message", (msg) => {
-    messageHistory.push(msg); //Save messages
+    messageHistory.push({ msg, messageCount }); //Save messages
+    messageCount++;
     if (msg === "/h" || msg === "/history") {
       io.emit("chat message", "---Previous Messages---");
       io.emit(
@@ -57,11 +38,36 @@ io.on("connection", (socket) => {
       ); //See previous messages
     } else {
       console.log("message:", msg);
-      io.emit("chat message", `👩🏻‍💻 user says: ${msg} at ${getTime()}`); //Show all messages
+      io.emit(
+        "chat message",
+        `${messageCount} - [👨🏻‍💻] user says: ${msg} at ${getTime()}`
+      ); //Show all messages
     }
   });
 });
 
+//Functions
+const getTime = () => {
+  let hours = new Date().getHours();
+  let minutes = new Date().getMinutes();
+  let seconds = new Date().getSeconds();
+
+  if (hours < 10) hours = "0" + hours;
+  if (minutes < 10) minutes = "0" + minutes;
+  if (seconds < 10) seconds = "0" + seconds;
+
+  return `${hours}:${minutes}:${seconds}`;
+};
+let messageHistory = [];
+const getMessageHistory = () => {
+  for (let i = 0; i <= messageHistory.length; i++) {
+    io.emit(
+      "chat message",
+      `[Chat Bot 🤖] ${JSON.stringify(messageHistory[i])} at ${getTime()}`
+    );
+  }
+  messageHistory = [];
+};
 http.listen(port, () => {
   console.log(`🚀 Chat running at http://localhost:${port}/`);
 });
